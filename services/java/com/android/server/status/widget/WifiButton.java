cbd17c9198962aba1c5a75d4114caba2280672d6
Seweryn Zeman <cad.mortess@gmail.com>
2011-02-01 02:10:43 +0100

    Long-press on Power Widget to open settings menu.

--- services/java/com/android/server/status/widget/WifiButton.java
+++ services/java/com/android/server/status/widget/WifiButton.java
@@ -7,10 +7,10 @@ import com.android.server.status.widget.StateTracker;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.content.Intent;
 import android.content.IntentFilter;
 import android.net.wifi.WifiManager;
 import android.os.AsyncTask;
+import android.os.Bundle;
 import android.util.Log;
 import android.provider.Settings;
 
@@ -159,6 +159,13 @@ public class WifiButton extends PowerButton{
         sWifiState.toggleState(context);
     }
 
+    public boolean launchActivity(Context context) {
+        Intent intentWifi = new Intent();
+        intentWifi.setAction(android.provider.Settings.ACTION_WIFI_SETTINGS);
+        intentWifi.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        context.startActivity(intentWifi);
+        return true;
+    }
 
     public static WifiButton getInstance() {
         if (ownButton == null) {
